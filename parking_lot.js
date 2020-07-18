9dfd298d694f70d029d7c18eef4b28e0797ae57f
krapjost <krapjost@gmail.com>
2020-07-18 11:06:46 +0900

    parking_lot_counter

--- parking_lot.js
+++ parking_lot.js
@@ -1,35 +1,32 @@
-let carAmount = 0;
-const carAmountMax = 5;
+var carAmount = 0 ;
+function addCar(){
+   const carAmountMax = 5;
+   if(carAmount<carAmountMax){
+    carAmount ++;
+   }else{carAmount=carAmountMax};
+    console.log(carAmount);
+    printCurIn();
+    function printCurIn(){
+        if(carAmount==carAmountMax){
+          alert ("만차입니다.");
+        }else if (carAmount==4) {
+          alert ("주차 1대 가능.");
+        }else if (carAmount==3) {
+          alert ("주차 2대 가능.");
+        }else{
+          alert ("환영합니다.");
+        }}};
 
-document.getElementById("carinbutton").onclick = addCar;
-document.getElementById("caroutbutton").onclick = delCar;
-
-//차 추가
-function addCar() {
-  carAmount += 1;
-  return carAmount;
-  function printCurIn() {
-    if(carAmount==carAmountMax){
-      alert ("만차입니다.")
-    }else if (carAmount==4) {
-      alert ("주차 1대 가능.")
-    }else if (carAmount==3) {
-      alert ("주차 2대 가능.")
-    }else{
-      alert ("환영합니다.")
-    }
-  }
-}
-
-//차 제거
-function delCar() {
-  carAmount -= 1;
-  return carAmount;
-  function printCurOut() {
-    if(carAmount==carAmountMax-1){
-      alert ("4대 안녕히 가세요.")
-    }else{
-      alert ("안녕히 가세요.")
-    }
-  }
-}
+function delCar(){
+  const carAmountMin = 0;
+  if (carAmount>carAmountMin){
+    carAmount -= 1;
+  }else{carAmount=carAmountMin};
+    console.log(carAmount);
+    printCurOut();
+      function printCurOut() {
+        if(carAmount>0){
+          alert ("안녕히가세요");
+        }else if(carAmount==0){
+          alert ("이제 아무도 없네요");
+        }}};
