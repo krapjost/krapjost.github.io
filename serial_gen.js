5192b019ddb50144a318857392a442030fe1fc4c
krapjost <krapjost@gmail.com>
2020-07-22 17:07:12 +0900

    update serial gen
    replace it to new functions

--- serial_gen.js
+++ serial_gen.js
@@ -1,86 +1,25 @@
-let serial = [];
-let serial.string = slice()
-let serial.number = makeNum()
 
-function slice(){
-  const str = document.getElementById('a').value
-  console.log(str);
-  const sliced = str.slice(0, 2);
-  console.log(sliced);
-};
+function a(){
+  let arr = [];
 
-function makeNum(){
-  const a = '000'+i;
-  for(let i=0; i<=){
-
+  while(true){
+    var product = prompt('product를 입력해주세요 : ');
+    var code="";
+    arr.push(product);
+    let count=0;
+    if(product=="q"){
+      alert('프로그램을 종료합니다.')
+      break;
+    }
+    let sStr = product.slice(0,2); //
+    code+=sStr;
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] == product) count++;
+  console.log(count);
+  }
+    if(count<10){
+      code+="000"+count;
+    }
+    alert('생성한 코드는 ' +code);
   }
 };
-let keyArr = [];
-let newArr = [{input:i}];
-for(let i=0000; i<keyArr.length; i++){
-  let key = keyArr[i];
-  serial[key] = newArr[i];
-};
-
-
-function slice(){
-  const str = document.getElementById('a').value
-  console.log(str);
-  const sliced = str.slice(0, 2);
-  console.log(sliced);
-};
-function num(){
-  const a = '0000';
-  const b = a ++;
-  if (strA=strB){
-
-  }
-};
-function serialGen(){
-  const a = slice().value + num().value
-}
-
-
-        var arr = [];
-        while(true){
-
-
-            var product = prompt("뭐 : ", "monitor");
-
-            var re_product = product.toLowerCase();
-
-            if(re_product == "q") {
-                break;
-            }
-
-            arr.push(re_product);
-
-            //console.log(product);
-
-            var sid01 = re_product.substring(0,2);
-            var sid02 = sid01.toUpperCase();
-
-            var count = 1
-            for(var i = 0; i < arr.length-1; i++){
-               if(arr[i] == re_product){
-                    count++;
-               }
-            }
-
-            //console.log(count);
-            if(0<count&&count<=9){
-                document.write(sid02 + "000" + count + "<br>");
-            }
-            else if(9<count&&count<=99){
-                document.write(sid02 + "00" + count + "<br>");
-            }
-            else if(99<count&&count<=999){
-                document.write(sid02 + "0" + count + "<br>");
-            }
-            else{
-                document.write(sid02 + count + "<br>");
-            }
-
-
-        }
-        */
